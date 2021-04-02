fa2f3af05f676188fbc4894a02e5a49bfab125ae
pratiksha <pratikshaladkat30@gmail.com>
2021-04-02 21:33:38 +0530

    [pratiksha] : add a new class :Demo1

---
+++ src/Sample/Demo1.java
@@ -0,0 +1,12 @@
+package Sample;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello...");
+		System.out.println("This is Pratiksha..");
+		System.out.println("I'm adding KiteZerodha project..");
+	}
+
+}
